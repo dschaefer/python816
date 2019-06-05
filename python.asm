9f2b9bef6b2b7de63376efab465a45012c156fef
Doug Schaefer <cdtdoug@gmail.com>
2019-06-05 00:10:28 -0400

    move print to it's own file

--- python.asm
+++ python.asm
@@ -33,7 +33,7 @@ start
     phd
     .swapStacks basic_stack, python_stack
 
-    ; set start up code object
+    ; - set start up code object -
     ; load the data bank register
     .a8
     lda #`doug
@@ -61,49 +61,8 @@ start
     .al
     .xl
 
-print .proc
-    ; B, X = pointer to string
-    pha
-    phy
-    ; save and set dp to 0
-    phd
-    lda #0
-    tcd
-    ; need to use basic stack for kernel call
-    .swapStacks python_stack, basic_stack
-    .a8
-loop
-    lda $0, b, x
-    beq done
-    ; prepare for kernel call
-    phx
-    phb
-    .x8
-    ; set bank to zero
-    ldy #0
-    phy
-    plb
-    ; call the kernel
-    .cpuemu
-    jsr $FFD2 ; chrout
-    .cpunat
-    ; put the bank back
-    .x16
-    plb
-    plx
-    ; next character
-    inx
-    bra loop
-done
-    .a16
-    .swapStacks basic_stack, python_stack
-    pld
-    ply
-    pla
-    rts
-    .pend
-
     .include "ops.asm"
+    .include "print.asm"
     .include "tests/doug.asm"
 
     .align $1000
